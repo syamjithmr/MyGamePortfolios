8c64ee1a7e14f5fe19946ab5df211e94fb943f70
syamjithmr <51257720+syamjithmr@users.noreply.github.com>
2020-11-27 11:33:24 +0530

    DanceFighter Source code first upload

---
+++ DanceFighter/Assets/Scripts/EthanController.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+
+//[Flags]
+public enum DanceMoves
+{
+    None,
+    Dance1,
+    Dance2,
+    Dance3
+}
+public enum FightMoves
+{
+    None,
+    Fight1,
+    Fight2,
+    Fight3
+}
+
+public class EthanController : MonoBehaviour
+{
+    Animator ethenAnimator;
+
+    float forwardRotationAngle;
+
+    public DanceMoves SelectedDanceMove;
+    public FightMoves SelectedFightMove;
+    public bool IsDancing { get; set; }
+    public bool IsFighting { get; set; }
+    public bool GameStarted { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        forwardRotationAngle = 1.0f;
+        ethenAnimator = GetComponent<Animator>();
+        SelectedDanceMove = DanceMoves.None;
+        IsFighting = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameStarted)
+        {
+            ethenAnimator.SetBool("Walking", Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D));
+            ethenAnimator.SetBool("Running", Input.GetKey(KeyCode.Space));
+            SetDanceAnimation();
+            SetFightAnimation();
+
+            if (Input.GetKey(KeyCode.W))
+                forwardRotationAngle = 0.0f;
+            if (Input.GetKey(KeyCode.A))
+                forwardRotationAngle = -90.0f;
+            if (Input.GetKey(KeyCode.S))
+                forwardRotationAngle = 180.0f;
+            if (Input.GetKey(KeyCode.D))
+                forwardRotationAngle = 90.0f;
+            if (forwardRotationAngle != 1.0f)
+            {
+                Vector3 cameraDirectionXZ = Vector3.ProjectOnPlane(transform.position - Camera.main.transform.position, transform.up);
+                Vector3 forwardRotatedXZ = Quaternion.AngleAxis(forwardRotationAngle, transform.up) * cameraDirectionXZ;
+                //print("Curr XZ positions: " + cameraDirectionXZ + ", Rotated XZ positions: " + forwardRotatedXZ);
+                transform.forward = forwardRotatedXZ.normalized;
+            }
+            forwardRotationAngle = 1.0f;
+        }
+    }
+
+    void SetDanceAnimation()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            if (!IsDancing && SelectedDanceMove == DanceMoves.Dance1)
+            {
+                ethenAnimator.SetBool("Dance1", true);
+            }
+            else if (!IsDancing && SelectedDanceMove == DanceMoves.Dance2)
+            {
+                ethenAnimator.SetBool("Dance2", true);
+            }
+            else if (!IsDancing && SelectedDanceMove == DanceMoves.Dance3)
+            {
+                ethenAnimator.SetBool("Dance3", true);
+            }
+        }
+    }
+
+    void SetFightAnimation()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            if (!IsFighting && SelectedFightMove == FightMoves.Fight1)
+            {
+                ethenAnimator.SetBool("Fight1", true);
+            }
+            else if (!IsFighting && SelectedFightMove == FightMoves.Fight2)
+            {
+                ethenAnimator.SetBool("Fight2", true);
+            }
+            else if (!IsFighting && SelectedFightMove == FightMoves.Fight3)
+            {
+                ethenAnimator.SetBool("Fight3", true);
+            }
+        }
+    }
+
+    public void SetDanceMove(int danceMove)
+    {
+        SelectedDanceMove = (DanceMoves)danceMove;
+    }
+
+    public void SetFightMove(int fightMove)
+    {
+        SelectedFightMove = (FightMoves)fightMove;
+    }
+
+    public void StartGame()
+    {
+        GameStarted = true;
+    }
+}
---
+++ DanceFighter/Assets/Scripts/EthanParentConstraint.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+
+public class EthanParentConstraint : MonoBehaviour
+{
+    public GameObject ethanLookAtObject;
+    public bool IsPaused { get; set; }
+    GameObject ethan;
+    Transform ethanLookAtTransform;
+    SkinnedMeshRenderer ethanMeshRenderer;
+
+    public float cameraSensitivity;
+    Vector3 cameraOffset;
+    Vector3 cameraClampMin;
+    Vector3 cameraClampMax;
+    Vector3 hitOffsetValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ethanLookAtTransform = ethanLookAtObject.transform;
+        ethan = GameObject.Find("Ethan");
+        ethanMeshRenderer = ethan.transform.Find("EthanBody").GetComponent<SkinnedMeshRenderer>();
+        cameraOffset = new Vector3(ethanLookAtTransform.position.x, ethanLookAtTransform.position.y, -3);
+        hitOffsetValue = new Vector3(0.2f, 0.2f, 0.2f);
+        cameraClampMin.y = ethanMeshRenderer.bounds.min.y + hitOffsetValue.y;
+        cameraClampMax.y = float.PositiveInfinity;
+        cameraClampMin.x = float.NegativeInfinity;
+        cameraClampMin.z = float.NegativeInfinity;
+        cameraClampMax.x = float.PositiveInfinity;
+        cameraClampMax.y = float.PositiveInfinity;
+        cameraClampMax.z = float.PositiveInfinity;
+        IsPaused = true;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsPaused)
+        {
+            Vector3 vectorFromEthan = ethanLookAtTransform.position - transform.position;
+            Quaternion verticalRotation = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * cameraSensitivity, Vector3.up);
+            Quaternion horizontalRotation = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * cameraSensitivity, Vector3.Cross(vectorFromEthan.normalized, Vector3.up));
+            cameraOffset = verticalRotation * horizontalRotation * cameraOffset;
+            transform.position = ethanLookAtTransform.position + cameraOffset;
+            vectorFromEthan = transform.position - ethanLookAtTransform.position;
+
+            Debug.DrawRay(ethanLookAtTransform.position, vectorFromEthan, Color.red);
+            //var hitArr = Physics.RaycastAll(transform.position - hitOffsetValue, vectorToEthan, vectorToEthan.magnitude - 1);
+            if (Physics.Raycast(ethanLookAtTransform.position, vectorFromEthan, out RaycastHit raytoEthanHit, vectorFromEthan.magnitude, ~LayerMask.GetMask("Ethan")))
+            {
+                //cameraClampMin.y = float.NegativeInfinity;
+                Debug.DrawRay(ethanLookAtTransform.position, raytoEthanHit.point - ethanLookAtTransform.position, Color.green);
+                Debug.DrawRay(raytoEthanHit.point, raytoEthanHit.normal, Color.blue);
+                Vector3 hitOffset = Vector3.Project(hitOffsetValue, raytoEthanHit.normal);
+                hitOffset *= hitOffset.magnitude / Vector3.Dot(hitOffsetValue, raytoEthanHit.normal);
+                transform.position = raytoEthanHit.point + hitOffset;
+            }
+
+            Vector3 currCameraPosXZ = new Vector3(transform.position.x, ethanLookAtTransform.position.y, transform.position.z);
+            //print("Curr XZ positions: " + currCameraPosXZ + ", Prev XZ positions: " + prevCameraPosXZ + ", Move Direction: " + cameraMoveDir);
+            Vector3 minDistFromEthan = ethanLookAtTransform.position + (currCameraPosXZ - ethanLookAtTransform.position).normalized;
+            Debug.DrawRay(ethanLookAtTransform.position, (new Vector3(transform.position.x, ethanLookAtTransform.position.y, transform.position.z) - ethanLookAtTransform.position).normalized, Color.yellow);
+            Debug.DrawLine(new Vector3(transform.position.x, ethanLookAtTransform.position.y - 0.1f, transform.position.z), ethanLookAtTransform.position - new Vector3(0.0f, 0.1f, 0.0f), Color.cyan);
+            if (transform.position.x > minDistFromEthan.x && transform.position.x < ethanLookAtTransform.position.x)
+            {
+                cameraClampMin.x = float.NegativeInfinity;
+                cameraClampMax.x = minDistFromEthan.x;
+            }
+            else if (transform.position.x < minDistFromEthan.x && transform.position.x > ethanLookAtTransform.position.x)
+            {
+                cameraClampMin.x = minDistFromEthan.x;
+                cameraClampMax.x = float.PositiveInfinity;
+            }
+            else
+            {
+                cameraClampMin.x = float.NegativeInfinity;
+                cameraClampMax.x = float.PositiveInfinity;
+            }
+            if (transform.position.z > minDistFromEthan.z && transform.position.z < ethanLookAtTransform.position.z)
+            {
+                cameraClampMin.z = float.NegativeInfinity;
+                cameraClampMax.z = minDistFromEthan.z;
+            }
+            else if (transform.position.z < minDistFromEthan.z && transform.position.z > ethanLookAtTransform.position.z)
+            {
+                cameraClampMin.z = minDistFromEthan.z;
+                cameraClampMax.z = float.PositiveInfinity;
+            }
+            else
+            {
+                cameraClampMin.z = float.NegativeInfinity;
+                cameraClampMax.z = float.PositiveInfinity;
+            }
+
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, cameraClampMin.x, cameraClampMax.x),
+                Mathf.Clamp(transform.position.y, cameraClampMin.y, cameraClampMax.y),
+                Mathf.Clamp(transform.position.z, cameraClampMin.z, cameraClampMax.z));
+            //print("Clamp positions: "+cameraClampMin+", "+cameraClampMax +", LookAt: " + ethanLookAtTransform.position + ", Transform: " +transform.position + ", MinDist: " + minDistFromEthan + ", Magnitude: " +(transform.position - ethanLookAtTransform.position).magnitude+"\n");
+            transform.LookAt(ethanLookAtTransform);
+        }
+    }
+}
