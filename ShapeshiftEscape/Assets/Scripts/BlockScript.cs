08d1d8000dac2f4005b3539f598ecd1f16cee20e
syamjithmr <51257720+syamjithmr@users.noreply.github.com>
2020-11-27 11:43:11 +0530

    ShapeshiftEscape Source Code First Upload

---
+++ ShapeshiftEscape/Assets/Scripts/BlockScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+    public GameObject blockExplosion;
+
+    Vector3 TunnelEndPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TunnelEndPos = GameObject.Find("TunnelEnd").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag(tag))
+        {
+            FindObjectOfType<Camera>().GetComponent<GameManager>().GameOver = true;
+            GameObject gameoverExplosion = Instantiate(blockExplosion, transform.position, new Quaternion());
+            gameoverExplosion.GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            if (transform.position.y > TunnelEndPos.y + 50)
+            {
+                FindObjectOfType<Camera>().GetComponent<GameManager>().Needblock = true;
+                FindObjectOfType<Camera>().GetComponent<GameManager>().destroyedBlockTagName = tag;
+            }
+            else
+                FindObjectOfType<Camera>().GetComponent<GameManager>().Needblock = false;
+
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ ShapeshiftEscape/Assets/Scripts/ConeScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ConeScript : MonoBehaviour
+{
+    ParticleSystem speedLines;
+    bool isSpeeding;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedLines = GetComponent<ParticleSystem>();
+        isSpeeding = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSpeeding)
+        {
+            if (speedLines.isStopped)
+            {
+                isSpeeding = false;
+            }
+        }
+    }
+
+    public void initiateSpeeding()
+    {
+        isSpeeding = true;
+    }
+}
